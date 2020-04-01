a1350a1d28b067f6ee185acb577543039c61e6b9
KH <kersingh@gmail.com>
2020-03-31 20:01:39 -0400

    Updates

---
+++ precache-manifest.e13b6564a7b9535a3c9a9937e77f30b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7718e97e8d114e4059c24c0d8ba46f20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b63e88f8571b023ec7f",
+    "url": "/static/css/main.28d2be93.chunk.css"
+  },
+  {
+    "revision": "8134e3650d426c115652",
+    "url": "/static/js/2.b4097d03.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b4097d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b63e88f8571b023ec7f",
+    "url": "/static/js/main.60fc9022.chunk.js"
+  },
+  {
+    "revision": "06c627e1b42e36d59b1c",
+    "url": "/static/js/runtime-main.32759b97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
