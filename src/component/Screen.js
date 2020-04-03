495b565055355975f9a8684197bfa9d1701d4098
Peter Boggis <codehackdays@googlemail.com>
2020-04-03 07:59:16 +0100

    Add mock up for instruction how to upload video

--- src/component/Screen.js
+++ src/component/Screen.js
@@ -7,6 +7,7 @@ class Screen extends React.Component {
     console.log(this.props.embedUrl);
 
     return (
+      <div>
       <Card>
         <Card.Body>
           <Card.Title>Hope TV</Card.Title>
@@ -18,6 +19,14 @@ class Screen extends React.Component {
           </iframe>
         </Card.Body>
       </Card>
+      <Card>
+        <Card.Body>
+          <Card.Text>
+            To see your video here upload to YouTube with hashtag ...
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      </div>
     );
   }
 }
