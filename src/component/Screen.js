4314907fc2b7556268d606bc21a6635f6c17e87b
Jermaine Oppong <jeropp00@gmail.com>
2020-04-04 01:37:24 +0100

    Implement functional accordion. Refactor app styles.

--- src/component/Screen.js
+++ src/component/Screen.js
@@ -1,23 +1,26 @@
 import React from 'react';
 
-import Card from 'react-bootstrap/Card';
+import ImgLogo from '../image/logo-transparent.png';
 
 class Screen extends React.Component {
   render(){
     console.log(this.props.embedUrl);
 
     return (
-      <Card>
-        <Card.Body>
-          <Card.Title>Hope TV</Card.Title>
-          <iframe width="100%" height="315" frameBorder="0"
-                  title="Hope TV"
-                  src={this.props.embedUrl}
-                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen>
-          </iframe>
-        </Card.Body>
-      </Card>
+      <>
+        <div class="text-center mt-5">
+          <img alt="Hug of the Soul logo" src={ImgLogo} width="400px" height="auto" />
+        </div>
+
+        <iframe width="100%"
+                height="315"
+                frameBorder="0"
+                title="Hope TV"
+                src={this.props.embedUrl}
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen>
+        </iframe>
+      </>
     );
   }
 }
