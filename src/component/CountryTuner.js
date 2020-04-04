9ce41de37473bbcc28a96eddbaf4da3b487f5b0d
Peter Boggis <codehackdays@googlemail.com>
2020-04-04 21:48:04 +0100

    Fix display name for country and language drop down buttons

--- src/component/CountryTuner.js
+++ src/component/CountryTuner.js
@@ -11,17 +11,27 @@ import usFlag from '../image/united_states_of_america_640.png';
 
 import './DropdownToggle.scss';
 
+import countries from '../data/countries.json';
+
 class CountryTuner extends React.Component {
   constructor(props){
     super(props);
 
     this.state = {
-      selectedCountry: "us"
+      selectedCountryCode: "us",
+      selectedCountryName: "United States"
     };
   }
 
   onSelectedCountryChange = (item) => {
-    this.setState({ selectedCountry: item },
+    var countryName = "";
+    for (var i in countries){
+      if (countries[i].code === item){
+        countryName = countries[i].name;
+      }
+    }
+
+    this.setState({ selectedCountryCode: item, selectedCountryName: countryName },
                   () => this.props.onSelectedCountryChange(item));
   }
 
@@ -30,14 +40,14 @@ class CountryTuner extends React.Component {
       <div className="hug-dropdown-toggle">
         <Dropdown onSelect={this.onSelectedCountryChange}>
           <Dropdown.Toggle variant="default">
-            {this.state.selectedCountry}
+            {this.state.selectedCountryName}
           </Dropdown.Toggle>
           <Dropdown.Menu>
             <Dropdown.Item eventKey="bo"><Image src={boFlag} height="20" /> Bolivia</Dropdown.Item>
             <Dropdown.Item eventKey="de"><Image src={deFlag} height="20" /> Germany</Dropdown.Item>
             <Dropdown.Item eventKey="in"><Image src={inFlag} height="20" /> India</Dropdown.Item>
             <Dropdown.Item eventKey="gb"><Image src={gbFlag} height="20" /> United Kingdom</Dropdown.Item>
-            <Dropdown.Item eventKey="us"><Image src={usFlag} height="20" /> United States of America</Dropdown.Item>
+            <Dropdown.Item eventKey="us"><Image src={usFlag} height="20" /> United States</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </div>
