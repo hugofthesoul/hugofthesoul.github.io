84170874c28158a13598689e950fda471736adc3
Peter Boggis <codehackdays@googlemail.com>
2020-04-04 18:36:10 +0100

    Add playlist structure so that it looks blank but good enough to be deployed (in case the rest of it takes time)

--- src/component/Playlist.js
+++ src/component/Playlist.js
@@ -1,17 +1,60 @@
 import React from 'react';
 
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 class Playlist extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      start: 0
+    };
+  }
+
   render(){
     return (
-      <Card>
-        <Card.Body>
-          <Card.Text>
-            Playlist
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <Container>
+        <Row>
+          <Col width="20%">
+            <Card>
+              <Card.Body>
+                <img />
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col width="20%">
+            <Card>
+              <Card.Body>
+                <img />
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col width="20%">
+            <Card>
+              <Card.Body>
+                <img />
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col width="20%">
+            <Card>
+              <Card.Body>
+                <img />
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col width="20%">
+            <Card>
+              <Card.Body>
+                <img />
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
