634bed0c64bb8c3a4a09a8ecd4806fffb3e5b37c
Peter Boggis <codehackdays@googlemail.com>
2020-04-04 15:13:08 +0100

    Have Hugs and Hospital pages match About page in terms of margins and title

--- src/Hugs.js
+++ src/Hugs.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 
 import './Hugs.scss'
 
 class Hugs extends React.Component {
   render(){
     return (
-      <div className="hugs">
-        <h2 className="title">Supporters</h2>
+      <Container className="hugs">
+        <h1 className="title">Supporters</h1>
         <Card>
           <Card.Text>Rommy would love to hear from you!</Card.Text>
           <Card.Subtitle><Button>Connect</Button></Card.Subtitle>
         </Card>
-      </div>
+      </Container>
     );
   }
 }
