61f992075f0a3adb175b776ea2cc38c09575474f
phyiction <kersingh@gmail.com>
2020-04-04 14:15:11 -0400

    Fix styling for website

--- src/component/NewsMarquee.js
+++ src/component/NewsMarquee.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-
 import "./NewsMarquee.scss";
 
 class NewsMarquee extends React.Component {
