1bb789edc1f74dcb17185f74c7f485ba4c6b1365
Peter Boggis <codehackdays@googlemail.com>
2020-04-04 01:14:49 +0100

    Fix bug with slider

--- src/component/VideoCountTuner.js
+++ src/component/VideoCountTuner.js
@@ -20,7 +20,8 @@ class VideoCountTuner extends React.Component {
       <Card>
         <Card.Body>
           <input type="range" min="1" max="20" step="1"
-                 value={this.state.selectedVideoCount} onChange={this.onSelectedVideoCountChange} />
+                 value={this.state.selectedVideoCount}
+                 onChange={changeEvent => this.onSelectedVideoCountChange(changeEvent.target.value)} />
         </Card.Body>
       </Card>
     );
