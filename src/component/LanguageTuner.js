269e65bb762e1342a4a69b8e71ef9f0e571b8dd8
Peter Boggis <codehackdays@googlemail.com>
2020-04-04 13:19:33 +0100

    Adjust country and language drop down lists

--- src/component/LanguageTuner.js
+++ src/component/LanguageTuner.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
 import Dropdown from 'react-bootstrap/Dropdown';
-import Image from 'react-bootstrap/Image';
-
-import enFlag from '../image/england_640.png';
-import deFlag from '../image/germany_640.png';
-import esFlag from '../image/spain_640.png';
 
 class LanguageTuner extends React.Component {
   constructor(props){
@@ -29,9 +24,9 @@ class LanguageTuner extends React.Component {
             {this.state.selectedLanguage}
           </Dropdown.Toggle>
           <Dropdown.Menu>
-            <Dropdown.Item eventKey="en"><Image src={enFlag} height="20" /></Dropdown.Item>
-            <Dropdown.Item eventKey="de"><Image src={deFlag} height="20" /></Dropdown.Item>
-            <Dropdown.Item eventKey="es"><Image src={esFlag} height="20" /></Dropdown.Item>
+            <Dropdown.Item eventKey="en">English</Dropdown.Item>
+            <Dropdown.Item eventKey="de">Deutsch</Dropdown.Item>
+            <Dropdown.Item eventKey="es">Español</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </div>
