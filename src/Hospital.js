c7f9f4291d2c2c4922c48546ef46e5646db86d49
Peter Boggis <codehackdays@googlemail.com>
2020-04-04 14:36:07 +0100

    Add team members to hospital wall

--- src/Hospital.js
+++ src/Hospital.js
@@ -9,12 +9,32 @@ class Hospital extends React.Component {
       <div className="hospital">
         <h2 className="title">@Hospital</h2>
         <Card>
-          <Card.Text>We care for you and we are praying for you</Card.Text>
-          <Card.Subtitle><i>- Rommy</i></Card.Subtitle>
+          <Card.Text>Let me know what you want me to put for you.</Card.Text>
+          <Card.Subtitle><i>- Jay</i></Card.Subtitle>
         </Card>
         <Card>
-          <Card.Text>We can give hope because we have hope</Card.Text>
+          <Card.Text>Let me know what you want me to put for you.</Card.Text>
+          <Card.Subtitle><i>- Jermaine</i></Card.Subtitle>
+        </Card>
+        <Card>
+          <Card.Text>Let me know what you want me to put for you.</Card.Text>
+          <Card.Subtitle><i>- Kersing</i></Card.Subtitle>
+        </Card>
+        <Card>
+          <Card.Text>Let me know what you want me to put for you. In German?</Card.Text>
+          <Card.Subtitle><i>- Mattias</i></Card.Subtitle>
+        </Card>
+        <Card>
+          <Card.Text>We give hope because we have hope.</Card.Text>
           <Card.Subtitle><i>- Peter</i></Card.Subtitle>
+        </Card>
+        <Card>
+          <Card.Text>Let me know what you want me to put for you.</Card.Text>
+          <Card.Subtitle><i>- Pratyoosh</i></Card.Subtitle>
+        </Card>
+        <Card>
+          <Card.Text>Let me know what you want me to put for you. In spanish?</Card.Text>
+          <Card.Subtitle><i>- Rommy</i></Card.Subtitle>
         </Card>
       </div>
     );
