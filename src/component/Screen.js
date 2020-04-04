73d096ed06aa761d8bf5f2d6f65053452ecc64cd
Jermaine Oppong <jeropp00@gmail.com>
2020-04-04 15:14:47 +0100

    Make logo background image of screen section

--- src/component/Screen.js
+++ src/component/Screen.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-import ImgLogo from '../image/logo-transparent.png';
+import './Screen.scss';
 
 class Screen extends React.Component {
-  render(){
+  render() {
     console.log(this.props.embedUrl);
 
     return (
-      <>
-        <div class="text-center mt-5">
-          <img alt="Hug of the Soul logo" src={ImgLogo} width="400px" height="auto" />
-        </div>
-
+      <div className="screen-section">
         <iframe width="100%"
                 height="315"
                 frameBorder="0"
@@ -20,7 +16,7 @@ class Screen extends React.Component {
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen>
         </iframe>
-      </>
+      </div>
     );
   }
 }
